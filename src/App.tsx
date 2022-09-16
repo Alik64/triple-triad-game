35ce9aaff24eab52634f462b4ff59a7a977ba4ac
Alik64 <albert.barsamov@gmail.com>
2022-09-16 10:07:32 +0200

    implement initial positioins of hands + board cells

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import React from "react";
 import s from "./App.module.css";
 
 function App() {
-  return <div className={s.root}></div>;
+  const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  return (
+    <div className={s.root}>
+      <div className={s.hands}></div>
+      <div className={s.board}>
+        {board.map((item, index) => (
+          <div key={index} className={s.cell}></div>
+        ))}
+      </div>
+      <div className={s.hands}></div>
+    </div>
+  );
 }
 
 export default App;
