3a7412ad61e9a86076d3fd954a3627d4ba66f78d
Alik64 <albert.barsamov@gmail.com>
2022-09-18 21:27:55 +0200

    implement fetch character

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import s from "./App.module.css";
+
 import Card from "./components/Card";
+import { Character } from "./interfaces";
+
+type JSONResponse = {
+  data: Array<Character>;
+  errors?: Array<{ message: string }>;
+};
 
 function App() {
   const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   const enemy = [0, 0, 0, 0, 0];
-  const player = [0, 0, 0, 0, 0];
+  const [player, setPlayer] = useState<Array<Character>>([]);
+
+  useEffect(() => {
+    const getPlayerCards = async () => {
+      const response = await fetch(
+        "http://localhost:3004/api/v1/marvel/create"
+      );
+      const characters: JSONResponse = await response.json();
+      console.log(characters.data);
+      setPlayer(characters.data);
+    };
+    getPlayerCards();
+  }, []);
+
   return (
     <div className={s.root}>
       <div className={s.hands}>
