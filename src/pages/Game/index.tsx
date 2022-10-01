7c632c8359d6e89afd43ca367b420db7b3ce9963
Alik64 <albert.barsamov@gmail.com>
2022-10-01 19:33:36 +0200

    implement launchGameThunk

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,46 +1,48 @@
 import React, { useEffect, useState } from "react";
-import cn from "classnames";
 
 import Hand from "../../components/Hand";
-
-import s from "./Game.module.css";
 import Board from "../../components/Board";
 import Modal from "../../components/Modal";
 import Preloader from "../../components/Preloader";
+
 import { Character } from "../../interfaces";
+
 import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../redux/store";
 import {
   getEnemyCardsThunk,
   getPlayerCardsThunk,
+  launchGameThunk,
+  setBoard,
+  setPlayerCards,
   toggleModal,
 } from "../../redux/characterSlice";
-import { AppDispatch } from "../../redux/store";
 
-type JSONResponse = {
-  data: Array<Character>;
-  errors?: Array<{ message: string }>;
-};
+import cn from "classnames";
+import s from "./Game.module.css";
 
 const Game: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
 
-  const player = useSelector((state: any) => state.characters.playerCards);
-  const enemy = useSelector((state: any) => state.characters.enemyCards);
-  const isLoading = useSelector((state: any) => state.characters.isLoading);
+  const player = useSelector(
+    (state: RootState) => state.characters.playerCards
+  );
 
-  const [board, setBoard] = useState<(Character | number)[]>([
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-  ]);
-  const [serverboard, setServerBoard] = useState<any[]>([
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-  ]);
+  const enemy = useSelector((state: RootState) => state.characters.enemyCards);
+  const isLoading = useSelector(
+    (state: RootState) => state.characters.isLoading
+  );
+
+  const board = useSelector((state: RootState) => state.characters.board);
+  const serverBoard = useSelector(
+    (state: RootState) => state.characters.serverBoard
+  );
 
   const [background, setBackground] = useState<boolean>(true);
 
-  // const [enemy, setEnemy] = useState<Array<Character>>([]);
-  // const [player, setPlayer] = useState<Array<Character>>([]);
-  // const [isLoading, setLoading] = useState<boolean>(false);
-  const [choiseCard, setChoiseCard] = useState<number | string | null>(null);
+  const [chosenCardId, setChosenCardId] = useState<number | string | null>(
+    null
+  );
   const [enemyScore, setEnemyScore] = useState<number>(0);
   const [playerScore, setPlayerScore] = useState<number>(0);
   const [winner, setWinner] = useState<string>("");
@@ -53,110 +55,95 @@ const Game: React.FC = () => {
   }, []);
 
   const handleHandsClick = (id: string | number) => {
-    setChoiseCard(id);
+    setChosenCardId(id);
   };
 
-  // const handleCellClick = async (index: number) => {
-  //   let params = {
-  //     hands: {
-  //       p1: player,
-  //       p2: enemy,
-  //     },
-  //     currentPlayer: "p1",
-  //     board: serverboard,
-  //     move: {},
-  //   };
+  const handleCellClick = async (index: number) => {
+    let params = {
+      hands: {
+        p1: player,
+        p2: enemy,
+      },
+      currentPlayer: "p1",
+      board: serverBoard,
+      move: {},
+    };
 
-  //   const playerCard = player.find((item) => item.id === choiseCard);
+    const playerCard = player.find((item) => item.id === chosenCardId);
 
-  //   if (playerCard) {
-  //     params.move = {
-  //       id: playerCard.id,
-  //       hits: playerCard.attacks[1],
-  //       position: index + 1,
-  //     };
-  //   } else {
-  //     return;
-  //   }
+    if (playerCard) {
+      params.move = {
+        id: playerCard.id,
+        hits: playerCard.attacks[1],
+        position: index + 1,
+      };
+    } else {
+      return;
+    }
 
-  //   setBoard((prevState) => {
-  //     if (playerCard) {
-  //       const copyState: (Character | number)[] = [...prevState];
-  //       copyState[index] = playerCard;
-  //       return copyState;
-  //     }
-  //     return prevState;
-  //   });
+    dispatch(setBoard({ index, playerCard }));
+    dispatch(setPlayerCards(chosenCardId));
+    setChosenCardId(null);
+    dispatch(launchGameThunk(params));
+    // STOP HERE
+    // const nextStep = await responseGame.json();
 
-  //   setPlayer((prevState) =>
-  //     prevState.filter((item) => item.id !== choiseCard)
-  //   );
-  //   setChoiseCard(null);
+    // setServerBoard(nextStep.board);
+    // setBoard(
+    //   nextStep.oldBoard.map((item: any) =>
+    //     typeof item === "object" ? { ...item.poke, holder: item.holder } : item
+    //   )
+    // );
 
-  //   const responseGame = await fetch(
-  //     "https://ttgapi.herokuapp.com/api/v1/marvel/game",
-  //     { method: "POST", body: JSON.stringify(params) }
-  //   );
+    // if (nextStep.move !== null) {
+    // setEnemy((prevState) =>
+    //   prevState.filter((item) => item.id !== nextStep.move.poke.id)
+    // );
 
-  //   const nextStep = await responseGame.json();
-  //   setServerBoard(nextStep.board);
-  //   setBoard(
-  //     nextStep.oldBoard.map((item: any) =>
-  //       typeof item === "object" ? { ...item.poke, holder: item.holder } : item
-  //     )
-  //   );
-
-  //   if (nextStep.move !== null) {
-  //     setEnemy((prevState) =>
-  //       prevState.filter((item) => item.id !== nextStep.move.poke.id)
-  //     );
-
-  //     setTimeout(() => {
-  //       setBoard(
-  //         nextStep.board.map((item: any) =>
-  //           typeof item === "object"
-  //             ? { ...item.poke, holder: item.holder }
-  //             : item
-  //         )
-  //       );
-  //       setPlayerScore(
-  //         nextStep.board.reduce((acc: number, item: any) => {
-  //           if (item.holder === "p1") {
-  //             acc++;
-  //           }
-  //           return acc;
-  //         }, 0)
-  //       );
-  //       setEnemyScore(
-  //         nextStep.board.reduce((acc: number, item: any) => {
-  //           if (item.holder === "p2") {
-  //             acc++;
-  //           }
-  //           return acc;
-  //         }, 0)
-  //       );
-  //     }, 500);
-  //   } else {
-  //     console.log(nextStep);
-  //     console.log("player pokes", nextStep.hands.p1.pokes.length);
-  //     console.log("enemy pokes", nextStep.hands.p2.pokes.length);
-  //     const nbP1 = nextStep.board.reduce((acc: number, item: any) => {
-  //       if (item.holder === "p1") {
-  //         acc++;
-  //       }
-  //       return acc;
-  //     }, 0);
-  //     const nbP2 = nextStep.board.reduce((acc: number, item: any) => {
-  //       if (item.holder === "p2") {
-  //         acc++;
-  //       }
-  //       return acc;
-  //     }, 0);
-  //     setPlayerScore(nbP1 + nextStep.hands.p1.pokes.length);
-  //     setEnemyScore(nbP2 + nextStep.hands.p2.pokes.length);
-  //     endgame();
-  //   }
-  // };
+    // setBoard(
+    //   nextStep.board.map((item: any) =>
+    //     typeof item === "object"
+    //       ? { ...item.poke, holder: item.holder }
+    //       : item
+    //   )
+    // );
+    //   setPlayerScore(
+    //     nextStep.board.reduce((acc: number, item: any) => {
+    //       if (item.holder === "p1") {
+    //         acc++;
+    //       }
+    //       return acc;
+    //     }, 0)
+    //   );
+    //   setEnemyScore(
+    //     nextStep.board.reduce((acc: number, item: any) => {
+    //       if (item.holder === "p2") {
+    //         acc++;
+    //       }
+    //       return acc;
+    //     }, 0)
+    //   );
+    // } else {
+    //   console.log(nextStep);
+    //   console.log("player pokes", nextStep.hands.p1.pokes.length);
+    //   console.log("enemy pokes", nextStep.hands.p2.pokes.length);
+    //   const nbP1 = nextStep.board.reduce((acc: number, item: any) => {
+    //     if (item.holder === "p1") {
+    //       acc++;
+    //     }
+    //     return acc;
+    //   }, 0);
+    //   const nbP2 = nextStep.board.reduce((acc: number, item: any) => {
+    //     if (item.holder === "p2") {
+    //       acc++;
+    //     }
+    //     return acc;
+    //   }, 0);
+    //   setPlayerScore(nbP1 + nextStep.hands.p1.pokes.length);
+    //   setEnemyScore(nbP2 + nextStep.hands.p2.pokes.length);
+    //   endgame();
+    // }
+  };
 
   function endgame() {
     console.log("player", playerScore + player.length);
@@ -177,10 +164,7 @@ const Game: React.FC = () => {
   return (
     <div className={cn(s.root, { [s.board2]: background })}>
       <Hand side="left" characters={enemy} disabled score={enemyScore} />
-      <Board
-        board={board}
-        //  onClick={handleCellClick}
-      />
+      <Board board={board} onClick={handleCellClick} />
       <Hand
         onClick={handleHandsClick}
         side="right"
@@ -200,7 +184,47 @@ const Game: React.FC = () => {
         >
           <span className={s.resetBtn_span}></span>Reset game
         </button>
-        <button onClick={() => dispatch(toggleModal())}>XXX</button>
+        <button
+          onClick={() =>
+            // dispatch(
+            //   setBoard({
+            //     index: 1,
+            //     playerCard: {
+            //       id: 1011078,
+            //       name: "Starjammers",
+            //       description: "",
+            //       thumbnail: {
+            //         path: "http://i.annihil.us/u/prod/marvel/i/mg/9/60/537f74ae55a3a.jpg",
+            //       },
+            //       resourceURI:
+            //         "http://gateway.marvel.com/v1/public/characters/1011078",
+            //       wiki: "http://marvel.com/universe/Starjammers?utm_campaign=apiRef&utm_source=d0522038ca5d2d32ec335cc0e418e27c",
+            //       urlCharacters: "http://marvel.com/characters/Starjammers",
+            //       attacks: [12, [4, 3, 2, 3]],
+            //       values: { top: 4, right: 3, bottom: 2, left: 3 },
+            //     },
+            //   })
+            // )
+            // dispatch(setPlayerCards(player[0].id))
+            dispatch(
+              launchGameThunk({
+                hands: {
+                  p1: player,
+                  p2: enemy,
+                },
+                currentPlayer: "p1",
+                board: serverBoard,
+                move: {
+                  id: player[0].id,
+                  hits: player[0].attacks[1],
+                  position: 1,
+                },
+              })
+            )
+          }
+        >
+          xxx
+        </button>
       </section>
     </div>
   );
